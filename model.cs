77c8ae886831bf5266001059dfa322f2880d8890
Jonesey86 <irj082024@gmail.com>
2025-02-03 09:24:54 +0100

    generel fixes meeting planner

--- model.cs
+++ model.cs
@@ -1,13 +1,11 @@
-using MeetingPlanner.Controller;
+using Meetingplanner.Controller;
 
-namespace MeetingPlanner.Models
-
-public class Meeting 
+namespace Meetingplanner.Models;
+public class Meeting
 {
     public string? Title { get; set; }
-    public string? Day { get; set; }
-    public List<string>? Participants { get; set; }
-    public TimeOnly StartTime { get; set; }
+    public string? Day {get; set;}
+    public List<string>? Participants { get; set; } 
+    public TimeOnly StartTime { get; set; } 
     public TimeOnly EndTime { get; set; }
-
-}
+};
--- program.cs
+++ program.cs
@@ -1,9 +1,48 @@
-﻿namespace New_Meeting_Planner;
+﻿﻿﻿using Meetingplanner.Controller;
+using Meetingplanner.View;
+
+namespace Meetingplanner;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var controller = new MeetingController();
+        var view = new MeetingView(controller);
+
+        while (true)
+        {
+            Console.WriteLine("Meeting planner");
+            Console.WriteLine("1.Display meetings");
+            Console.WriteLine("2.Add new meetings");
+            Console.WriteLine("3.Search for participant");
+            Console.WriteLine("4.Exit");
+            Console.WriteLine("Choose an option: ");
+            string option = Console.ReadLine()?.Trim() ?? "1";
+
+            switch (option)
+            {
+                case "1":
+                    Console.Clear();
+                    view.DisplayMeetings();
+                    break;
+                case "2":
+                    Console.Clear();
+                    view.AddMeeting();
+                    break;
+                case "3":
+                    Console.Clear();
+                    view.SearchMeetingsParticipant();
+                    break;
+                case "4":
+                    Console.Clear();
+                    return;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Invalid. Try again");
+                    Console.ReadLine();
+                    break;
+            }
+        }
     }
-}
+};
